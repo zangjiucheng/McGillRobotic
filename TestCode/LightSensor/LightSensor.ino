11c4220f3069bad748af5f5adf151ed828a6a090
Jiucheng Zang <zangjiucheng@gmail.com>
2023-03-11 14:48:48 -0500

    Light

--- TestCode/LightSensor/LightSensor.ino
+++ TestCode/LightSensor/LightSensor.ino
@@ -9,11 +9,12 @@
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  pinMode(A0, INPUT);
 }
 
 void loop() {
   // reads the input on analog pin A0 (value between 0 and 1023)
-  int analogValue = analogRead(A2);
+  int analogValue = analogRead(A0);
 
   Serial.print("Analog reading: ");
   Serial.print(analogValue); // the raw analog reading
